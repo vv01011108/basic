ddd2282937dd3f6fd7459821583f60b45894f9ec
seonga <vv01011108@gmail.com>
2024-10-30 23:37:30 +0900

    first

---
+++ src/test/java/org/example/basic1/Basic1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.basic1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Basic1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
