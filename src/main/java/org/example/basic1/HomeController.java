0d9c5e0881749e3c93199dad470688711c925fd0
seonga <vv01011108@gmail.com>
2024-10-31 22:21:13 +0900

    @RequestParam

--- src/main/java/org/example/basic1/HomeController.java
+++ src/main/java/org/example/basic1/HomeController.java
@@ -2,6 +2,7 @@ package org.example.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // @Controller 의미
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // 아래 있는 HomeController가 컨트롤러이다.
 @Controller
 public class HomeController {
+
+    // 전역변수로 빼왔기 때문에 새로고침할 때마다 숫자 카운트 됨
     private int count;
 
     public HomeController() {
@@ -43,5 +46,15 @@ public class HomeController {
     public int showIncrease() {
         count++;
         return count;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    // @RequestParam의 의미
+    // 개발자가 스프링부트에게 말한다.
+    // int a는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
+    // @RequestParam은 생략 가능
+    public int showPlus(@RequestParam(defaultValue = "0")int a, @RequestParam(defaultValue = "0")int b) {
+        return a + b;
     }
 }
